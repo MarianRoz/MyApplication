265b64964c1718be9481f24fc4134cc92dc2efb5
DESKTOP-097O910\rozum <rozumnyi.marian@gmail.com>
2024-02-03 10:44:46 +0200

    Added interface for empty service

---
+++ MyApplication2/Services/IAnnouncementService.cs
@@ -0,0 +1,12 @@
+﻿namespace MyApplication2.Services
+{
+    public interface IAnnouncementService
+    {
+        void AddAnnouncement(string announcement);
+        void EditAnnouncement(string announcement);
+        void DeleteAnnouncement(string announcement);
+        void SeeAnnouncement(string announcement);
+        void ShowAnnouncement(string announcement);
+
+    }
+}

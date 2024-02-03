8507759a8188301f1d49bde44ad01d5e8eeb7879
DESKTOP-097O910\rozum <rozumnyi.marian@gmail.com>
2024-02-03 10:42:53 +0200

    Added empty services

---
+++ MyApplication2/Services/AnnouncementService.cs
@@ -0,0 +1,11 @@
+﻿namespace MyApplication2.Services
+{
+    public class AnnouncementService : IAnnouncementService
+    {
+        public void AddAnnouncement(string announcement) { }
+        public void EditAnnouncement(string announcement) { }
+        public void DeleteAnnouncement(string announcement) { }
+        public void SeeAnnouncement(string announcement) { }
+        public void ShowAnnouncement(string announcement) { }
+    }
+}

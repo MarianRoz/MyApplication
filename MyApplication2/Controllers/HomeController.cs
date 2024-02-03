c63cda5b27fde52d5d7dc7e252ea987476bb355f
DESKTOP-097O910\rozum <rozumnyi.marian@gmail.com>
2024-02-03 10:51:33 +0200

    Added controller for empty services

---
+++ MyApplication2/Controllers/HomeController.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MyApplication2.Services;
+using System.Diagnostics;
+
+namespace MyApplication2.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly IAnnouncementService _service;   
+        public HomeController(IAnnouncementService service)
+        {
+                _service = service;
+        }
+        [HttpGet]
+        [Route("AddAnnouncement")]
+        public void AddAnnouncement(string announcement) { }
+        [HttpGet]
+        [Route("EditAnnouncement")]
+        public void EditAnnouncement(string announcement) { }
+        [HttpGet]
+        [Route("DeleteAnnouncement")]
+        public void DeleteAnnouncement(string announcement) { }
+        [HttpGet]
+        [Route("SeeAnnouncement")]
+        public void SeeAnnouncement(string announcement) { }
+        [HttpGet]
+        [Route("ShowAnnouncement")]
+        public void ShowAnnouncement(string announcement) { }
+    }
+}
